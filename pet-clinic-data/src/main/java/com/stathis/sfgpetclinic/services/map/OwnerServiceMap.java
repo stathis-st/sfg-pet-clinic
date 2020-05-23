63bef3c29b0736d168e3c680d9f8305c925b10cc
stathis.st <stathis.stamatis@gmail.com>
2020-05-23 21:59:08 +0300

    Impl Map based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,36 @@
+package com.stathis.sfgpetclinic.services.map;
+
+import com.stathis.sfgpetclinic.model.Owner;
+import com.stathis.sfgpetclinic.services.CrudService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,36 @@
+package com.stathis.sfgpetclinic.services.map;
+
+import com.stathis.sfgpetclinic.model.Vet;
+import com.stathis.sfgpetclinic.services.CrudService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
