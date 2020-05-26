e8deb4af0ac4c97942106240b58ebb3b0d9c66fa
stathis.st <stathis.stamatis@gmail.com>
2020-05-27 00:14:29 +0300

    Added PetTypes to bootstrap data loader. Closes #32

--- pet-clinic-web/src/main/java/com/stathis/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/stathis/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.stathis.sfgpetclinic.bootstrap;
 
 import com.stathis.sfgpetclinic.model.Owner;
+import com.stathis.sfgpetclinic.model.PetType;
 import com.stathis.sfgpetclinic.model.Vet;
 import com.stathis.sfgpetclinic.services.OwnerService;
+import com.stathis.sfgpetclinic.services.PetTypeService;
 import com.stathis.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
