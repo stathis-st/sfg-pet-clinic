7c51712252befc53bb3eb475b068bf6646529a3b
stathis.st <stathis.stamatis@gmail.com>
2020-05-27 21:32:36 +0300

    Added Spring Data JPA service for Specialty. Closes #44

--- pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/map/PetTypeMapService.java
@@ -1,31 +1,31 @@
 package com.stathis.sfgpetclinic.services.map;
 
-import com.stathis.sfgpetclinic.model.Specialty;
-import com.stathis.sfgpetclinic.services.SpecialtyService;
+import com.stathis.sfgpetclinic.model.PetType;
+import com.stathis.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
 
     @Override
-    public Set<Specialty> findAll() {
+    public Set<PetType> findAll() {
         return super.findAll();
     }
 
     @Override
-    public Specialty findById(Long id) {
+    public PetType findById(Long id) {
         return super.findById(id);
     }
 
     @Override
-    public Specialty save(Specialty object) {
+    public PetType save(PetType object) {
         return super.save(object);
     }
 
     @Override
-    public void delete(Specialty object) {
+    public void delete(PetType object) {
         super.delete(object);
     }
 
--- pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/map/SpecialtyMapService.java
@@ -1,31 +1,31 @@
 package com.stathis.sfgpetclinic.services.map;
 
-import com.stathis.sfgpetclinic.model.Pet;
-import com.stathis.sfgpetclinic.services.PetService;
+import com.stathis.sfgpetclinic.model.Specialty;
+import com.stathis.sfgpetclinic.services.SpecialtyService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
 
     @Override
-    public Set<Pet> findAll() {
+    public Set<Specialty> findAll() {
         return super.findAll();
     }
 
     @Override
-    public Pet findById(Long id) {
+    public Specialty findById(Long id) {
         return super.findById(id);
     }
 
     @Override
-    public Pet save(Pet object) {
+    public Specialty save(Specialty object) {
         return super.save(object);
     }
 
     @Override
-    public void delete(Pet object) {
+    public void delete(Specialty object) {
         super.delete(object);
     }
 
