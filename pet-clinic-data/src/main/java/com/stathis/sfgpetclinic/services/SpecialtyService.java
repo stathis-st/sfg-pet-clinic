d1867cf3d4d401e5f3c93b526918565c09843a1b
stathis.st <stathis.stamatis@gmail.com>
2020-05-27 10:10:06 +0300

    Added Specialty Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.stathis.sfgpetclinic.services;
+
+import com.stathis.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,36 @@
+package com.stathis.sfgpetclinic.services.map;
+
+import com.stathis.sfgpetclinic.model.Specialty;
+import com.stathis.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
