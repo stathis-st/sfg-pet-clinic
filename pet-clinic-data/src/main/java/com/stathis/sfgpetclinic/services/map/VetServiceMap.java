e483f8aca3c4e738dc3419bf55c06d487b865f80
stathis.st <stathis.stamatis@gmail.com>
2020-05-27 10:47:25 +0300

    Added Specialties to vets on startup. Closes #34

--- pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.stathis.sfgpetclinic.services.map;
 
+import com.stathis.sfgpetclinic.model.Specialty;
 import com.stathis.sfgpetclinic.model.Vet;
+import com.stathis.sfgpetclinic.services.SpecialtyService;
 import com.stathis.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -21,6 +29,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+
+        if (object.getSpecialties().size() > 0) {
+            object.getSpecialties().forEach(specialty -> {
+                if (specialty.getId() == null) {
+                    Specialty savedSpecialty = specialtyService.save(specialty);
+                    specialty.setId(savedSpecialty.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
