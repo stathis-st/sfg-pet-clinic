38f735b505946f53f1080583403100ed8ccf88c3
stathis.st <stathis.stamatis@gmail.com>
2020-06-30 19:31:14 +0300

    Implements find Owner. Closes #54

--- pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package com.stathis.sfgpetclinic.repositories;
 import com.stathis.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/stathis/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package com.stathis.sfgpetclinic.services;
 
 import com.stathis.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
