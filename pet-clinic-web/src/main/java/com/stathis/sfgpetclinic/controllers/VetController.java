cd23ceda8198d1b803b5158f3e93ae8227676e0d
stathis-st <stathis.stamatis@gmail.com>
2020-08-26 20:33:36 +0300

    Adds json endpoint for Vets. Closes #67

--- pet-clinic-web/src/main/java/com/stathis/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/stathis/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.stathis.sfgpetclinic.controllers;
 
+import com.stathis.sfgpetclinic.model.Vet;
 import com.stathis.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +25,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+
+        return vetService.findAll();
     }
 }
